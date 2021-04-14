16554494c746f980daecbbe2633ae79376754318
shuklahimanshu81 <70198636+shuklahimanshu81@users.noreply.github.com>
2021-04-14 07:02:01 +0530

    feat: added node.js code

---
+++ quiz.js
@@ -0,0 +1,32 @@
+var readlineSync = require('readline-sync')
+var chalk = require('chalk')
+var score=0;
+var score2 = 5;
+
+var userName = readlineSync.question('What is your name? ');
+console.log(chalk.red('Welcome! ' + userName + ' to A quiz on 13 Reasons why?  '));
+function play(question,answer){
+  var userAnswer = readlineSync.question(chalk.yellow(question))
+  
+  if(userAnswer.toUpperCase() === answer.toUpperCase()){
+    console.log('yes! you are right. ')
+    score = score+1;
+  }
+    else{-+-
+      console.log('oops! you are wrong. ')
+      score = score-1;
+    }
+    console.log('And, your score is ', score);
+    console.log('_____________')
+}
+var questions = [{question: 'What was the name of Clay jensens mom ',
+answer: 'Lainie Jensen',}, {
+  question: 'In which game Bryce walker was co-captain of team?  ' , answer: 'baseball',},{ question: 'In which state of USA Liberty high school was situated?  ', answer: 'california',},{ question: 'How many tapes did Hannah created ?  ',answer: '13',},{question: 'In which tape clay jensen was mentioned? ' ,answer: '11',},{ question: 'In which movie theater Hannah and clay used to work together? ', answer: 'Crestmont',}]
+  for(var i=0; i< questions.length; i++){
+    var curr = questions[i];
+    play(curr.question,curr.answer);
+  }
+  console.log('your score is '+ score);
+  console.log('Highest Score --> Himanshu shukla = 6')
+  console.log('Second Highest Score - to be announced soon')
+  
